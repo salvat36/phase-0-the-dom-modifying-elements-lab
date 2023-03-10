0e9c58d555828b92e67f0e1e6d3292faeef91c62
Ryan <salvat36@gmail.com>
2023-03-10 04:02:42 -0600

    4 passing

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const mainID = document.getElementById("main").remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Ryan-Salvato is the champion";
